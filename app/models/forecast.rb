4f3aa27696bf5bd101b794d873d04604acbcfc64
numsweb <numsweb@users.noreply.github.com>
2024-04-28 15:00:35 -0400

    changes required to add a test to verify fetching a temperature works successfully

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -2,30 +2,59 @@ class Forecast < ApplicationRecord
 
 
   def find_and_save_forecast(location)
-    #expects location (see format below)
+    #NOTE: There is a limit to how many requests can be made
+    # Test will fail when response is
+    #   <Net::HTTPTooManyRequests 429 Too Many Requests readbody=true>
+    # Specifically, the plan includes:
+    #
+    #    500 requests per day
+    #
+    #     25 requests per hour
+    #
+    #     3 requests per second
+
+
     require 'uri'
     require 'net/http'
 
-    #location would need to be collected from the data entered by the user.
+    #location would need to be collected from the data entered by the user. Ideally they would enter a city,state
+    # and this program would use an API to retrieve the latitude and longitude needed by the
+    # weather API
+
     # just stub a location for now lat,long for Aiken, SC
-    location = [33.566574, -81.719398]
+    if location.blank?
+      location = [33.566574, -81.719398]
+    end
 
-    url = URI("https://api.tomorrow.io/v4/weather/forecast?location=33.566574,-81.719398&apikey=wGr1PdeMGQzCZBNFpJItrdoOQANlEkzY")
 
+    static_url = URI("https://api.tomorrow.io/v4/weather/forecast?location=33.566574,-81.719398&apikey=wGr1PdeMGQzCZBNFpJItrdoOQANlEkzY")
+    url = URI("https://api.tomorrow.io/v4/weather/forecast?location=" + location[0].to_s + "," + location[1].to_s + "&apikey=wGr1PdeMGQzCZBNFpJItrdoOQANlEkzY")
+
+    #puts "url: " + url.inspect
+    #puts "built_url: " + built_url.inspect
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
 
     request = Net::HTTP::Get.new(url)
 
     response = http.request(request)
+    #puts response.inspect
+    if response.message.include?("Too Many Requests")
+      raise "Evaluation API access::Too many requests error, please try again later"
+    else
+      hash_response = JSON.parse response.body.gsub('=>', ':')
+      puts hash_response.inspect
+      puts hash_response["timelines"]["minutely"][0]["time"].inspect rescue puts "no time"
+      puts hash_response["timelines"]["minutely"][0]["values"]["temperature"].inspect rescue puts "no temperature"
+      time = hash_response["timelines"]["minutely"][0]["time"]
+      temperature = hash_response["timelines"]["minutely"][0]["values"]["temperature"]
+      forecast = Forecast.create(location:location.to_s,temperature: temperature.to_s, last_refreshed: time )
+      forecast.save
+      return [:time => time, :temperature => temperature]
+    end
 
-    hash_response = JSON.parse response.body.gsub('=>', ':')
-    time = hash_response["timelines"].first.second[0]["time"]
-    temperature = hash_response["timelines"].first.second[0]["values"]["temperature"]
 
-    forecast = Forecast.create(location:location.to_s,temperature: temperature, last_refreshed: time )
-    forecast.save
-    return [:time => time, :temperature => temperature]
+
 
   end
 
--- test/models/forecast_test.rb
+++ test/models/forecast_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ForecastTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # note api is throttled so cannot run too many tests at one time
+  # when it fails repeat the test
+   test "retrieve forecast" do
+     location = [33.566574, -81.719398]
+     forecast = Forecast.new
+     response = forecast.find_and_save_forecast(location)
+     assert(response.is_a?(Array))
+     assert(response[0].is_a?(Hash))
+     assert(response[0].has_key?(:time))
+     assert(response[0].has_key?(:temperature))
+   end
 end
